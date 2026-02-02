5df6719a01ec586beaa67d783a1ede35d7d71066
nikotroqe <108827315+nikotroqe@users.noreply.github.com>
2026-02-02 21:33:19 +0100

    Delete home directory

--- home/home.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("Bazaar360 Home loaded successfully");
-
-  const exploreBtn = document.querySelector(".btn-danger");
-
-  exploreBtn.addEventListener("click", () => {
-    alert("360° Tour coming soon 🚀");
-  });
-});
